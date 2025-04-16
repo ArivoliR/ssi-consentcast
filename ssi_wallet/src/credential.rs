5b3dda92288e875f345a0b62be61c3a510f34a27
Abhinav I S <abhinavis2005@gmail.com>
2025-04-16 19:15:39 +0530

    Basic Design

---
+++ ssi_wallet/src/credential.rs
@@ -0,0 +1,95 @@
+use bbs::{keys::PublicKey, prelude::Signature};
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+#[derive(Serialize, Deserialize, Debug)]
+
+pub struct CredentialSubject {
+    pub id: String, //users did
+    pub name: String,
+    pub dob: String,
+    pub address: String,
+    pub pan: String,
+}
+impl CredentialSubject {
+    pub fn generate_messages_vec(&self) -> Vec<String> {
+        vec![
+            self.id.clone(),
+            self.name.clone(),
+            self.dob.clone(),
+            self.address.clone(),
+            self.pan.clone(),
+        ]
+    }
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct VerifiableCredential {
+    pub id: String,
+    pub issuer: String, // "the bank"
+    pub issuance_date: String,
+    pub credential_subject: CredentialSubject,
+    pub signature: Signature,
+    pub public_key: PublicKey,
+}
+
+impl VerifiableCredential {
+    pub fn new(
+        id: String,
+        name: String,
+        dob: String,
+        address: String,
+        pan: String,
+        issuer: String,
+        issuance_date: String,
+    ) -> Self {
+        let (public_key, secret_key) = super::generate_bbs_keypair(5);
+        let credential_subject = CredentialSubject {
+            id: id.clone(),
+            name,
+            dob,
+            address,
+            pan,
+        };
+        let messages = credential_subject.generate_messages_vec();
+        let signature = super::sign_messages(&messages, &public_key, &secret_key);
+        VerifiableCredential {
+            id,
+            issuer,
+            issuance_date,
+            credential_subject,
+            signature,
+            public_key,
+        }
+    }
+}
+
+pub fn save_vc_to_file(vc: &VerifiableCredential, path: &str) -> std::io::Result<()> {
+    let json = serde_json::to_string_pretty(vc).expect("Failed to serialize Verifiable Credential");
+
+    let mut file = File::create(Path::new(path))?;
+    file.write_all(json.as_bytes())?;
+
+    Ok(())
+}
+#[cfg(test)]
+mod tests {
+    use crate::credential::save_vc_to_file;
+
+    use super::VerifiableCredential;
+
+    #[test]
+    pub fn generate_verifiable_credential() {
+        let verifiable_credential = VerifiableCredential::new(
+            "1".to_string(),
+            "arivoli".to_string(),
+            "23-11-2005".to_string(),
+            "IITM".to_string(),
+            "ABC1234".to_string(),
+            "Indian Bank".to_string(),
+            "16.04.2025".to_string(),
+        );
+        save_vc_to_file(&verifiable_credential, "./vc");
+        println!("{:?}", verifiable_credential);
+    }
+}
---
+++ ssi_wallet/src/lib.rs
@@ -0,0 +1,124 @@
+pub mod credential;
+
+use bbs::{
+    self,
+    issuer::Issuer,
+    keys::{PublicKey, SecretKey},
+    prelude::Signature,
+    prover::Prover,
+    ProofNonce, ProofRequest, SignatureMessage, SignatureProof,
+};
+
+fn to_fixed_32_bytes(input: &String) -> [u8; 32] {
+    let mut bytes = [0u8; 32]; // initialize with 0s
+    let input_bytes = input.as_bytes(); // convert String to &[u8]
+    let len = input_bytes.len().min(32); // handle strings longer than 32
+    bytes[..len].copy_from_slice(&input_bytes[..len]); // copy
+    bytes
+}
+
+pub fn generate_bbs_keypair(num_messages: usize) -> (bbs::keys::PublicKey, bbs::keys::SecretKey) {
+    bbs::issuer::Issuer::new_keys(num_messages).expect("Could not generate keys")
+}
+
+pub fn sign_messages(
+    messages: &[String],
+    public_key: &PublicKey,
+    secret_key: &SecretKey,
+) -> Signature {
+    let signature_messages: Vec<SignatureMessage> = messages
+        .iter()
+        .map(|msg| SignatureMessage::from(to_fixed_32_bytes(msg)))
+        .collect();
+
+    Issuer::sign(&signature_messages, secret_key, public_key).expect("Could Not Sign Message")
+}
+
+pub fn signed_pok_generate(
+    reveal_msg_indices: &[usize],
+    proof_request: &ProofRequest,
+    nonce: ProofNonce,
+    messages: &[String],
+    signature: Signature,
+) -> SignatureProof {
+    // Given the messages to be revealed, proof of request, nonce, messages and signature, generate
+    // proof of signature for the verifier
+    let signature_messages: Vec<SignatureMessage> = messages
+        .iter()
+        .map(|msg| SignatureMessage::from(to_fixed_32_bytes(msg)))
+        .collect();
+
+    let proof_messages: Vec<bbs::messages::ProofMessage> = signature_messages
+        .iter()
+        .enumerate()
+        .map(|(i, msg)| {
+            if reveal_msg_indices.contains(&i) {
+                bbs::messages::ProofMessage::Revealed(msg.clone())
+            } else {
+                bbs::messages::ProofMessage::Hidden(
+                    bbs::messages::HiddenMessage::ProofSpecificBlinding(msg.clone()),
+                )
+            }
+        })
+        .collect();
+    let pok = Prover::commit_signature_pok(proof_request, &proof_messages, &signature).unwrap();
+    let challenge_hash = Prover::create_challenge_hash(&[pok.clone()], None, &nonce).unwrap();
+
+    Prover::generate_signature_pok(pok, &challenge_hash).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use bbs::prelude::Verifier;
+
+    use super::*;
+
+    #[test]
+    fn generate_keys() {
+        let keys = generate_bbs_keypair(5);
+        println!("{:?}", keys);
+    }
+
+    #[test]
+    fn sign_message() {
+        let name: String = "Arivoli".to_string();
+        let age: String = "69".to_string();
+        let messages = &[name, age];
+        let (public_key, secret_key) = generate_bbs_keypair(2);
+        let sign = sign_messages(messages, &public_key, &secret_key);
+        println!("{:?}", sign);
+    }
+    #[test]
+    fn verify_message() {
+        let name: String = "Arivoli".to_string();
+        let age: String = "69".to_string();
+        let messages = &[name.clone(), age];
+
+        let (public_key, secret_key) = generate_bbs_keypair(2);
+        let sign = sign_messages(messages, &public_key, &secret_key);
+
+        // company sends over a proof requet and nonce
+        let revealed_message_indices = &[0];
+        let proof_request =
+            Verifier::new_proof_request(revealed_message_indices, &public_key).unwrap();
+        let nonce = Verifier::generate_proof_nonce();
+
+        let signature_proof = signed_pok_generate(
+            revealed_message_indices,
+            &proof_request,
+            nonce,
+            messages,
+            sign,
+        );
+
+        //verify
+        let verified_messages =
+            Verifier::verify_signature_pok(&proof_request, &signature_proof, &nonce).unwrap();
+
+        for message in verified_messages {
+            let bytes = message.to_bytes_uncompressed_form();
+            println!("{:?}", bytes);
+            assert_eq!(to_fixed_32_bytes(&name), bytes);
+        }
+    }
+}
